1656900cf11473c57010bd51f75765e3c3199a31
joefavergel <joseph@guane.com.co>
2024-02-03 22:16:28 -0500

    :wrench: Add environment variable loader for API keys

--- llmir/__init__.py
+++ llmir/__init__.py
@@ -1,1 +1,10 @@
 __version__ = "v0.1.0"
+
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv(dotenv_path="../.env")
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
